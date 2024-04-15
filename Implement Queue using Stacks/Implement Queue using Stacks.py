0d62b66e8d2ea0876c9dbf824109cb46aece62cd
vpanchyshyn <149326833+vpanchyshyn@users.noreply.github.com>
2024-04-15 15:28:11 +0300

    Solved: used two stacks to work with queue

---
+++ Implement Queue using Stacks/Implement Queue using Stacks.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, item, next=None):
+        self.item = item
+        self.next = next
+
+class Stack:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def push(self, item):
+        self.head = Node(item, self.head)
+
+    def pop(self):
+        item = self.head.item
+        self.head = self.head.next
+        return item
+
+    @property
+    def peek(self):
+        return self.head.item
+
+    def __len__(self):
+        count = 0
+        current = self.head
+        while current is not None:
+            count +=1
+            current = current.next
+        return count
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def push(self, x: int) -> None:
+        self.stack1.push(x)
+
+    def pop(self) -> int:
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.peek
+
+    def empty(self) -> bool:
+        return self.stack1.is_empty() and self.stack2.is_empty()
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
